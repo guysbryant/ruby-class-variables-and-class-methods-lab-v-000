a7e68893c0df95557d5e2d4eda360163c8392b1e
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-22 15:37:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,4 +18,20 @@ class Song
   def self.count
     @@count
   end
+  
+  def self.artists
+    
+  end
+  
+  def self.genres
+    
+  end
+  
+  def self.genre_count
+    
+  end
+  
+  def self.artist_count
+    
+  end
 end
