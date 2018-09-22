0303209db6fab90bce36956e971163d6655fa44f
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-22 15:34:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,11 @@
 class Song
   
   attr_reader :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
