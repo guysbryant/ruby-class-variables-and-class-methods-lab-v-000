237df18827abbedbe4652de6fe0c089f7742c674
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-22 15:56:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
   
   def self.genre_count
     genres = {}
-    @@genres.each {|genre| genres[genre] == nil? genres[genre] = 0 : genres[genre] += 1}
+    @@genres.each {|genre| genres[genre] == nil ? genres[genre] = 0 : genres[genre] += 1}
   end
   
   def self.artist_count
