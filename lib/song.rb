80635b117da3338307102dbf93688f98154a9f92
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-22 15:39:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
   
   def self.artists
     unique_artists = []
-    @@artists.each {|artist| unique_artists << artist if !unique_artists.include?(artist)
+    @@artists.each {|artist| unique_artists << artist if !unique_artists.include?(artist)}
     unique_artists
   end
   
